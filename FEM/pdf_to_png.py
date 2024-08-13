d8d337b71d23a0b5d265649bc1e4ec06bd95f36f
SADPR <aresdepargas@gmail.com>
2024-08-13 19:10:24 +0200

    Weak form

--- FEM/pdf_to_png.py
+++ FEM/pdf_to_png.py
@@ -2,10 +2,10 @@ from pdf2image import convert_from_path
 import os
 
 # Set the path to your PDF file
-pdf_path = 'StrongForm.pdf'
+pdf_path = '1D_Burgers_Equation_ROMs.pdf'
 
 # Create a directory to save PNG images
-output_folder = 'output_images'
+output_folder = 'media'
 os.makedirs(output_folder, exist_ok=True)
 
 # Convert PDF to images
@@ -13,7 +13,7 @@ images = convert_from_path(pdf_path)
 
 # Save each page as a PNG file
 for i, image in enumerate(images):
-    image_path = os.path.join(output_folder, f'StrongForm.png')
+    image_path = os.path.join(output_folder, f'WeakForm.png')
     image.save(image_path, 'PNG')
 
 print(f'PDF converted to PNG images and saved in "{output_folder}" directory.')
